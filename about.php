bfd1c13d945e9e01ac0eaf840a9b9a69cbc34acd
Bernard Bolter <b@bernardbolter.com>
2014-02-05 18:07:49 -0800

    finished about page

--- about.php
+++ about.php
@@ -8,15 +8,18 @@ Template Name: about
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-<div class="gallery_header">
-	
-</div>
-<div class="about_title_wrap clearfix">
-	<p class="about_head">ABOUT</p>
-	<div class="about_desc"><?php the_content(); ?></div>
+<div class="about_wrap">
+	<section class="layout">
+		<div class="about_left">
+			<p class="about_head">ABOUT</p>
+			<div class="about_desc"><?php the_content(); ?></div>
+		</div>
+		<div class="about_right">
+			<img src="<?php bloginfo( 'template_url' ); ?>/gfx/about.jpg" />
+		</div>
+	</section>
 </div>
 
 <?php endwhile; ?>
-
 
 <?php get_footer(); ?>
