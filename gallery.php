b83055f2aa7030918364c7a7eaf6ed37d8da6b31
Bernard Bolter <b@bernardbolter.com>
2014-02-06 01:49:12 -0800

    initial work on gallery page

--- gallery.php
+++ gallery.php
@@ -8,21 +8,20 @@ Template Name: gallery
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-<div class="gallery_wrap clearfix">
-	<div class="gallery_main_inner"><a href="<?php bloginfo( 'url' ); ?>/bench" title="BENCH"><img src="<?php bloginfo( 'template_url' ); ?>/gfx/gallery/bench/bench_square.jpg" alt="BENCH" /></a>
+<div class="gallery_wrap">
+	<div><a href="<?php bloginfo( 'url' ); ?>/six-legs-bench" title="SIX LEGS BENCH"><img src="<?php bloginfo( 'template_url' ); ?>/gfx/gallery/six-legs-bench/six-legs-bench_square.jpg" alt="SIX LEGS BENCH" /></a>
 	</div>
-	<div class="gallery_main_inner"><a href="<?php bloginfo( 'url' ); ?>/curved-back-chair" title="CURVED BACK CHAIR"><img src="<?php bloginfo( 'template_url' ); ?>/gfx/gallery/curved-back-chair/curved_back_chair_square.jpg" alt="CURVED BACK CHAIR"></a>
+	<div><a href="<?php bloginfo( 'url' ); ?>/captains-chair" title="CAPTAIN'S CHAIR"><img src="<?php bloginfo( 'template_url' ); ?>/gfx/gallery/captains-chair/captains-chair_square.jpg" alt="CAPTAIN'S CHAIR"></a>
 	</div>
-	<div class="gallery_main_inner"><a href="<?php bloginfo( 'url' ); ?>/splayed-leg-table" title="SPLAYED LEG TABLE"><img src="<?php bloginfo( 'template_url' ); ?>/gfx/gallery/splayed-leg-table/splayed_leg_table_square.jpg" alt="SPLAYED LEG TABLE"></a>
+	<div><a href="<?php bloginfo( 'url' ); ?>/splayed-leg-table" title="SPLAYED STAR TABLE"><img src="<?php bloginfo( 'template_url' ); ?>/gfx/gallery/splayed-star-table/splayed-star-table_square.jpg" alt="SPLAYED STAR TABLE"></a>
 	</div>
-	<div class="gallery_main_inner"><a href="<?php bloginfo( 'url' ); ?>/colum-footed-bench" title="COLUM FOOTED BENCH"><img src="<?php bloginfo( 'template_url' ); ?>/gfx/gallery/colum-footed-bench/colum_footed_bench_square.jpg" alt="COLUM FOOTED BENCH"></a>
+	<div><a href="<?php bloginfo( 'url' ); ?>/column-bench" title="COLUMN BENCH"><img src="<?php bloginfo( 'template_url' ); ?>/gfx/gallery/column-bench/column-bench_square.jpg" alt="COLUMN BENCH"></a>
 	</div>
-	<div class="gallery_main_inner"><a href="<?php bloginfo( 'url' ); ?>/table" title="TABLE"><img src="<?php bloginfo( 'template_url' ); ?>/gfx/gallery/table/table_square.jpg" alt="TABLE"></a>
+	<div><a href="<?php bloginfo( 'url' ); ?>/column-table" title="COLUMN TABLE"><img src="<?php bloginfo( 'template_url' ); ?>/gfx/gallery/column-table/column-table_square.jpg" alt="COLUMN TABLE"></a>
 	</div>
-	<div class="gallery_main_inner"><a href="<?php bloginfo( 'url' ); ?>/chair" title="CHAIR"><img src="<?php bloginfo( 'template_url' ); ?>/gfx/gallery/chair/chair_square.jpg" alt="CHAIR"></a>
+	<div><a href="<?php bloginfo( 'url' ); ?>/high-back-chair" title="HIGH BACK CHAIR"><img src="<?php bloginfo( 'template_url' ); ?>/gfx/gallery/high-back-chair/high-back-chair_square.jpg" alt="HIGH BACK CHAIR"></a>
+	</div>
 </div>
 
 <?php endwhile; ?>
-
-
 <?php get_footer(); ?>
